242ddce1843f0c3aed8f84fa18d11507283887c2
Emma Lundström <57397365+neut0ne@users.noreply.github.com>
2020-04-18 13:03:00 +0200

    Create randtext.go

---
+++ cmd/randtext.go
@@ -0,0 +1,16 @@
+package main
+
+//import "fmt"
+
+//func main() {
+//  var str1 string =`
+//    TODO thing
+//
+//    This is the first page of the TODO thing.
+//    I'm really just experimenting right now.
+//    Learning strings in golang and stuff.
+//    Let's see how that worked out.
+//    `
+//  println(str1)
+//}
+© 2020 GitHub, Inc.
