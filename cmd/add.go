dcbeaced7d37b1daacb3cdac6f23fb7616ab38ec
Emma Lundström <57397365+neut0ne@users.noreply.github.com>
2020-04-28 12:26:00 +0200

    Update add.go

--- cmd/add.go
+++ cmd/add.go
@@ -9,7 +9,23 @@ var addCmd = &cobra.Command{
   Use:   "add",
   Short: "Adds a task to your task list.",
   Run: func(cmd *cobra.Command, args []string) {
-    fmt.Println("Add called")
+    // In the first for loop, cobra takes i to be the index number of each added
+    // "args". arg orders the args in a range, with the first arg first, aso.
+    // Spaces delimits args. Add args WITH spaces by closing them in with double
+    // quotes.
+    // Ex: This will be three args: todo add wash the dishes
+    // Ex: This will be one arg: todo add "wash the dishes"
+    // Here's how to write:
+    // For i and arg, where arg is a range of args:
+    // Print i, then ":", then the arg.
+    // for i, arg := range args {
+    //   fmt.Println(i, ":", arg)
+    // }
+
+    // But we rather want to skip the quotes, and can live with adding only one
+    // task at a time, so we'll do this:
+    task := strings.Join(args, " ")
+    fmt.Printf("Added \"%s\" to your task list. \n", task)
   },
 }
 
