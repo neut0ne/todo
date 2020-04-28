4c6b6cc732132fbdd468b28e0dc0b69b0fd6ffe8
Emma Lundström <emma.lundstrom@unomaly.com>
2020-04-28 14:27:53 +0200

    initiated db

--- db/main.go
+++ db/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+  "github.com/boltdb/bolt"
+  "time"
+)
+
 func main() {
   db, err := bolt.Open("my.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
   if err != nil {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/neut0ne/todo
 
 go 1.14
 
-require github.com/spf13/cobra v1.0.0
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/spf13/cobra v1.0.0
+)
